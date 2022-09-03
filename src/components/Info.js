983a79c89a958d5f667fe4043cbf5d0b24228fb7
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2022-09-04 00:58:42 +0200

    ...

--- src/components/Info.js
+++ src/components/Info.js
@@ -9,14 +9,14 @@ export const Info = () => {
       <p>On the site below you can get some more information about my doings. It was the absolutely first site ever, that I was creating, and it is filled with crea(c)tivity.</p>
     </div>
 
-    <p className='point'>&#8226;</p>
+  {/*   <p className='point'>&#8226;</p> */}
     
 
     <div className='siteBox'>
             
-      <a href='https://metatronics-wunderland.netlify.app/start.html' target="_blank" rel="noopener noreferrer">      
+      <a className='link' href='https://metatronics-wunderland.netlify.app/start.html' target="_blank" rel="noopener noreferrer">      
         <img className='tut' src='./pics/tut.png' alt='tut' />
-        <p>&#10940; &#8226; klick here &#8226; &#10939;</p>    
+        <p className='klick'>&#10940; &#8226; klick here &#8226; &#10939;</p>    
       </a>
       
     </div>
