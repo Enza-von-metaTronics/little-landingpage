7a6c7203447be3ca071651bc7298aedfbb95027c
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2026-09-02 21:03:35 +0200

    Update PageHome.js
    abstand des paragrafen in zeile 21 nach oben vergrössert.

--- src/components/PageHome.js
+++ src/components/PageHome.js
@@ -17,7 +17,9 @@ export const PageHome = () => {
       <video width="720px" height="auto" controls poster="../pics/vorschauPic.jpg">
        <source src="schoenheit.mp4" type="video/mp4" />
       </video>
-<p>Diese Website wird derzeit neu entdeckt und weiterentwickelt.</p>
+  
+<p style={{ marginTop: "70px" }}>Diese Website wird derzeit neu entdeckt und weiterentwickelt.</p>
+  
       <div className='banner'>
         <h1>come to LinkedIn:</h1>
         <a href='https://www.linkedin.com/in/claudia-hinze-27656b180/' target="_blank" rel="noopener noreferrer">
